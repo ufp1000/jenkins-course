31a6a0f14a44304513ed08ac1990e923d8b8e4ee
ufp1000 <ufp_1000@rediffmail.com>
2019-12-20 23:29:07 +0530

    ufp1000
    added to run from user and not root

---
+++ scripts/install_jenkins_sudo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# this script is only tested on ubuntu xenial
+
+# install docker
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+sudo apt-get update
+sudo apt-get install -y docker-engine
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG docker ubuntu
+
+# run jenkins
+sudo mkdir -p /var/jenkins_home
+sudo chown -R 1000:1000 /var/jenkins_home/
+sudo docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
+
+# show endpoint
+sudo echo 'Jenkins installed'
+sudo echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
